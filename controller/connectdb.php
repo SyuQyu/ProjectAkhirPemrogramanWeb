07cfb32eeaaa42f1830ecb6e1cae283db0c9296b
Pandu <104115882+SyuQyu@users.noreply.github.com>
2023-05-12 21:56:51 +0700

    conn

--- controller/connectdb.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = new mysqli('localhost','root','','wecare');
-
-if ($conn->connect_error) {
-   die("Error: There is something error".$conn->connect_error);
-} else {
-    echo "Database connect successfuly";
-}
-?>
