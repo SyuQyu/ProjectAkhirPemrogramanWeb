2dc8e280f91628128bd015f86395057bf8415c6d
salazars01 <90942445+salazars01@users.noreply.github.com>
2023-05-20 20:48:38 +0700

    pandu gw udah gold

--- frontend/view/template/header.php
+++ frontend/view/template/header.php
@@ -1,17 +1,18 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="../../css/header.css">
+	<link rel="stylesheet" href="../../css/coba.css">
 </head>
 <body>
    <section class="header">
-    <a href="home.php" class="logo">Doctor.</a>
+    <a href="home.php" class="logo">weCare</a>
 
     <nav class="navbar">
-        <a href="">home</a>
-        <a href="">about</a>
-        <a href="">package</a>
-        <a href="">booking</a>
+        <a href="home.php">home</a>
+        <a href="homeAbout.php">about us</a>
+        <a href="homepackages.php">booking</a>
     </nav>
+
+
 </section>
 </body>
