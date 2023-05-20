b180dbc3083f3223d5db5d43ec70f1ebee5036c9
RizqGyx <mhdrizky32@gmail.com>
2023-05-20 21:31:24 +0700

    Reviews1

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -14,3 +14,21 @@ loadMoreBtn.onclick = () => {
         loadMoreBtn.style.display = 'none';
     }
 }
+
+var swiper = new Swiper(".reviews-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoHeight: true,
+    grabCursor: true,
+    breakpoints: {
+        640: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+      },
+});
